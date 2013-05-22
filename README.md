e6faf81ad96da7ddbe5698b922904822c3a8985f
Olexandr Shalakhin <olexandr@shalakhin.com>
2013-05-22 16:40:08 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# DODO check domains available in .com
+
+Simple usage:
+
+```bash
+dodo bamboo
+
+[NO] bamboo.com
+```
+
+or check text in files:
+
+```bash
+dodo -f domains_to_test.txt
+
+[NO] bamboo.com
+[NO] google.com
+[YES] example.com
+```
---
+++ dodo.go
@@ -0,0 +1,65 @@
+// Checkdomains is a tiny console application to check quickly if domain name is
+// already reserved
+package main
+
+import (
+    "fmt"
+    "bufio"
+    "log"
+    "flag"
+    "os"
+    "os/exec"
+    "regexp"
+
+    // "github.com/wsxiaoys/terminal"
+    "github.com/wsxiaoys/terminal/color"
+)
+
+var (
+    file, name string
+)
+
+func init() {
+    flag.StringVar(&file, "f", "", "path to the file with a list of domains to check")
+}
+
+func main() {
+    flag.Parse()
+    args := flag.Args()
+    switch {
+    case len(args) > 0:
+        for i:=0; i < len(args); i++ {
+            check(args[i])
+        }
+    case file != "":
+        handle(file)
+    }
+}
+
+func check(domain string) (result string) {
+    domain = fmt.Sprint(domain, ".com")
+    output, err := exec.Command("whois", domain).Output()
+    if err != nil {
+        log.Fatal(err)
+    }
+    re := regexp.MustCompile("No match")
+    ending := fmt.Sprint("\t", domain, "\n")
+    if re.FindString(string(output)) != "" {
+        color.Print("@g[OK]", ending)
+    } else {
+        color.Print("@r[NO]", ending)
+    }
+    return
+}
+
+func handle(file string) {
+    filename, err := os.Open(file)
+    if err != nil {
+        log.Fatal(err)
+    }
+    scanner := bufio.NewScanner(filename)
+    for scanner.Scan() {
+        check(scanner.Text())
+    }
+    defer filename.Close()
+}
